142799e2728a6aabccc166840812ca769dbdd000
mcverry <ryan.mcverry@gmail.com>
2015-04-19 16:16:37 -0400

    using lib for more advanced asset control

---
+++ src/assets.js
@@ -0,0 +1,12 @@
+function getAssets(){
+    return [
+            'res/img/balloon_orange.png',
+            'res/img/balloon_yellow.png',
+            'res/img/focus_splash.png',
+            'res/img/spritesheet_earth.png',
+            'res/img/balloon_pink.png',
+            'res/img/flashlight.png',
+            'res/img/hubble.png',
+            'res/vid/empire.mp4'
+    ]   
+}
